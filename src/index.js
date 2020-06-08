3c47e7edced960867ef4de5cb9b81305d4e51300
BartzDev <61787162+BartzDev@users.noreply.github.com>
2020-06-08 16:33:39 -0300

    Exercises 1.1.-1.2.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name='Fundamentals of React' exercises={props.exercises[0]}/>
+      <Part name='Using props to pass data' exercises={props.exercises[1]}/>
+      <Part name='State of a component' exercises={props.exercises[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+  )
+}
+
+const App = () => {
+  const exercises = [10, 7, 14]
+  return (
+    <div>
+      <Header course='Half Stack application development'/>
+      <Content exercises={exercises}/>
+      <Total exercises={exercises}/>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
